f3a9af53edc7fd93653d6a58926bbc142b27fc09
betzfer <miguelrodriguesdeoliveirasenai@gamil.com>
2023-08-25 08:33:41 -0300

    final

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -1,17 +1,22 @@
-import SocialLinks from "../sociallinks/socialLinks";
+// para utilizar o componete, crie o import na página que deseja
+// coloque o nome da pessoa entre <footer>NomePessoa</footer>
+// passe a informação dentro da tag Footer - LINK
+// <Footer> link={}>Karol-ss</Footer>
+import SocialLinks from "../socialLinks/socialLinks";
 import "./footer.module.css";
-import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js";
+import "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js";
 
 const Footer = ({ children, link }) => {
   return (
     <footer>
       <p>
         Feito com <ion-icon name="heart" /> por{" "}
-        <a href={link} target="_blank">
+        <a href={link} target="_blank" rel="noopener noreferrer">
           {children}
         </a>
+        </p>
         <SocialLinks icon={"logo-github"} href={"#"} />
-      </p>
+      
     </footer>
   );
 };
